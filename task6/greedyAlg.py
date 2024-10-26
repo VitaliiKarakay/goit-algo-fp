53d58dc65a13470634532f6c7122610f934bc70a
v.karakai <v.karakai@pin-up.tech>
2024-10-26 13:00:13 +0300

    task6 - implemented greedy alg

---
+++ task6/greedyAlg.py
@@ -0,0 +1,27 @@
+def greedy_algorithm(items, budget):
+    sorted_items = sorted(items.items(), key=lambda x: x[1]['calories'] / x[1]['cost'], reverse=True)
+    total_cost = 0
+    total_calories = 0
+    selected_items = []
+
+    for item, info in sorted_items:
+        if total_cost + info['cost'] <= budget:
+            selected_items.append(item)
+            total_cost += info['cost']
+            total_calories += info['calories']
+
+    return selected_items, total_calories
+
+
+# Приклад використання
+items = {
+    "pizza": {"cost": 50, "calories": 300},
+    "hamburger": {"cost": 40, "calories": 250},
+    "hot-dog": {"cost": 30, "calories": 200},
+    "pepsi": {"cost": 10, "calories": 100},
+    "cola": {"cost": 15, "calories": 220},
+    "potato": {"cost": 25, "calories": 350}
+}
+
+budget = 100
+print(greedy_algorithm(items, budget))
